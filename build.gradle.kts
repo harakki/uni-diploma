f99bd741aa9f8b3b2a5a71f894065b7743dfc381
harakki <124341753+harakki@users.noreply.github.com>
2025-12-28 13:54:18 +0700

    Fix: http requests errors

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-security-oauth2-client")
 
     // Spring Modulith
     implementation("org.springframework.modulith:spring-modulith-starter-core")

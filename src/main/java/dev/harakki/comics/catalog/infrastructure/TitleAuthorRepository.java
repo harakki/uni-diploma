1ef92fae3f9ea42e2daec85bd31807863a56183d
harakki <124341753+harakki@users.noreply.github.com>
2026-01-18 14:28:10 +0700

    Add: query to retrieve max sort order by title ID

--- src/main/java/dev/harakki/comics/catalog/infrastructure/TitleAuthorRepository.java
+++ src/main/java/dev/harakki/comics/catalog/infrastructure/TitleAuthorRepository.java
@@ -2,6 +2,7 @@ package dev.harakki.comics.catalog.infrastructure;
 
 import dev.harakki.comics.catalog.domain.TitleAuthor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
@@ -9,6 +10,7 @@ import java.util.UUID;
 @Repository
 public interface TitleAuthorRepository extends JpaRepository<TitleAuthor, UUID> {
 
+    @Query("SELECT COALESCE(MAX(ta.sortOrder), -1) FROM TitleAuthor ta WHERE ta.title.id = :titleId")
     int findMaxSortOrderByTitleId(UUID titleId);
 
 }
